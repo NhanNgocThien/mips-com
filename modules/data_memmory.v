2bd31d5f157432002e356313344b3a1afeef4171
NhanNgocThien <nhanthien1012@gmail.com>
2019-05-26 22:03:37 +0700

    data_memory updated

--- modules/data_memmory.v
+++ modules/data_memmory.v
@@ -3,23 +3,27 @@ module data_memory(
   input[31:0] address,
   input[31:0] data_in,
   input mem_read,
-  input mem_write
+  input mem_write,
+  input clk,
+  input reset
 );
-  reg [31:0] data_temp;
   reg [31:0] data_memory [0:255]; // Initialize 256 words of 32-bit memory location
   
-  assign data_out = data_temp;  
+  assign data_out = (mem_read == 1'b1)? data_memory[address] : 32'bx;
 
   integer x;
   initial begin
 	for(x = 0; x < 256; x = x + 1) begin
-		data_memory[x] = 32'd0;
+		data_memory[x] <= 32'd0;
 	end
   end
 
-  always@(address, data_in) begin
-	if(mem_read == 1'b1) data_temp <= data_memory[address];
-	if(mem_write == 1'b1) data_memory[address] <= data_in;
+  always@(posedge clk, posedge reset) begin
+	if(reset == 1'b1) begin
+		for(x = 0; x < 256; x = x + 1)
+			data_memory[x] <= 32'd0;
+	end
+	else if(mem_write == 1'b1) data_memory[address] <= data_in;
   end
 
 endmodule
