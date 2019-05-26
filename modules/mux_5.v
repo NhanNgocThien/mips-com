3cc876f62e361605b20158461be25010b78c6394
NhanNgocThien <nhanthien1012@gmail.com>
2019-05-26 22:05:52 +0700

    mux_5 and mux_32 updated

--- modules/mux_5.v
+++ modules/mux_5.v
@@ -4,6 +4,14 @@ module mux_5(
   input [4:0] in_2,
   input select
 );
-  assign out = (select == 1'b1)? in_2 : in_1;
+  reg [4:0] out_select;
+  assign out = out_select;
+  always@(*) begin
+	casex(select)
+		1'b0: out_select = in_1;
+		1'b1: out_select = in_2;
+		1'bx: out_select = 5'bx;
+	endcase
+  end
 
 endmodule
